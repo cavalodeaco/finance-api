c4cf5244cc646643bc35ad48bb246dafa515f4ae
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-09-18 16:21:51 -0300

    feat: update yaml

--- node-js/src/routers/record.js
+++ node-js/src/routers/record.js
@@ -16,11 +16,27 @@ const requestMiddleware = require("../middleware/request-middleware.js");
  *         - category
  *         - source_pay
  *       properties:
- *         amount:
+ *          amount:
  *           type: number
  *           description: Amount of the finance record
- *       example:
- *         amount: 10.00
+ *          "description":
+ *              type: string
+ *              description: Description of the finance record
+ *          type_pay:
+ *              type: string
+ *              description: Type of finance record like money, credit card, debit card, etc.
+ *          source_pay:
+ *              type: string
+ *              description: Type of source like money, card 1, card 2, etc.
+ *          category:
+ *              type: string
+ *              description: Category of the finance record like home, school, vehicle, etc.
+ *          due_date:
+ *              type: date
+ *              description: Due date to finance record. Required to finance record of type output.
+ *          in_date:
+ *              type: date
+ *              description: Input date of finance record. Required to finance record of type input.
  */
 
 const route = express.Router();
@@ -32,14 +48,26 @@ const route = express.Router();
  *  description: Records of finance
  *
  * /record/:type:
- *  post:
- *   summary: Create a new record
- *   tags: [Record]
- *   requestBody:
- *      required: true
- *      content:
- *          application/json
- *
+ * post:
+ *  summary: Create a new record of :type (input or output).
+ *  tags: [Record]
+ *  requestBody:
+ *   required: true
+ *   content:
+ *    application/json:
+ *     schema:
+ *      $ref: '#/components/schemas/Record'
+ *   responses:
+ *    200:
+ *     description: The created finance record.
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Record'
+ *    400:
+ *     description: Bad request
+ *    500:
+ *     description: Some server error
  */
 route.post("/:type", rescue(requestMiddleware), rescue(controller.post));
 
